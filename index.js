dc86838552918768ffb35267b126bf78be457318
Yitzchok Kimelman <itchkimel@gmail.com>
2021-04-13 00:24:36 -0400

    commit tree 76f54c7cbfaf853adda86c0d98aaefde85d206b1

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Add your code here
+let body = document.querySelector('body')
+let newDiv = document.createElement('div')
+body.append(newDiv)
+
+function submitData(userName, userEmail){
+  return fetch('http://localhost:3000/users',{
+  method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: (userName),
+      email: (userEmail)
+    })})
+    .then(resp => resp.json()
+    )
+    .then((objData) => {
+      for(const key in objData){
+        let returnedId = objData[key]
+        // console.log(returnedId)
+        let newLi = document.createElement('li')
+        newLi.innerText = returnedId
+        newDiv.appendChild(newLi)
+      }
+      // debugger
+    })
+  }
+  submitData()
+
+
+
+
+
+
+
+
+
+
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// };
+// fetch('http://localhost:3000/dogs', configurationObject)
